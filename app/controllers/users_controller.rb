afc8217f03ff4df0c4e830169d3e457590996145
Benjamin Li <email": null>
2017-03-13 16:06:34 -0400

    Create controller for creating user and shows error messages if it fails

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,18 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      session[:expire_after] = 60
+      redirect_to '/'
+    else
+      @errors = @user.errors.full_messages
+      render 'new'
+    end
+  end
+
   private
     def user_params
       params.require(:user).permit(:username, :email, :password)
