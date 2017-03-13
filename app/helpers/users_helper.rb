31cfe6028231f1f53dbc58e0d9bccdcb77463927
Benjamin Li <email": null>
2017-03-13 16:50:59 -0400

    Create helper methods for users

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,21 @@
 module UsersHelper
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  def login?
+    !!current_user
+  end
+
+  def require_user
+    redirect_to '/session/login' unless current_user
+  end
+
+  def authorized?
+    session[:user_id] == @portfolio.user_id
+  end
+
+  def authorize_user
+    redirect_to '/' unless authorized?
+  end
 end
