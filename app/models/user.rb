e500a3ff72db366d536ff118fc33c47ecb7c317c
Benjamin Li <email": null>
2017-03-13 15:28:09 -0400

    Add user migration and model files

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ApplicationRecord
+  validates :username, :email, presence: true
+  validates :email, uniqueness: true
+  validates :password, length: {minimum: 6}
+
+  has_secure_password
+end
