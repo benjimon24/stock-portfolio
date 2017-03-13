d19b54a36428af9a05cb4ec321e3e2ee61415e16
Benjamin Li <email": null>
2017-03-13 15:46:19 -0400

    Create user controller and user sign up form

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,10 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:username, :email, :password)
+    end
+end
