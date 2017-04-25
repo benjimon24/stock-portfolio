7748cf0f9a9dff26fd7f474538cf32d06fbddc8c
benjimon24 <benji102491@gmail.com>
2017-04-24 23:01:33 -0400

    Add stock info to portfolio index and show pages

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -4,19 +4,23 @@ class Portfolio < ApplicationRecord
     has_many :stocks, dependent: :destroy
 
     def current_value
-      self.stocks.inject(0) {|total, stock| total += stock.current_value}
+      stocks.inject(0) {|total, stock| total += stock.current_value}
     end
 
     def cost_basis
-      self.stocks.inject(0) {|total, stock| total += stock.cost_basis}
+      stocks.inject(0) {|total, stock| total += stock.cost_basis}
     end
 
     def net_profit
-      self.stocks.inject(0) {|total, stock| total += stock.net_profit}
+      stocks.inject(0) {|total, stock| total += stock.net_profit}
     end
 
     def percent_change
-      self.net_profit / self.cost_basis
+      if cost_basis == 0
+        return 0
+      else
+        (net_profit / cost_basis * 100).round(2)
+      end
     end
 
 end
--- app/models/stock.rb
+++ app/models/stock.rb
@@ -2,23 +2,26 @@ class Stock < ApplicationRecord
   validates :symbol, :buy_price, :volume, presence: true
   belongs_to :portfolio
 
+  def buy(quantity)
+  end
+
   def current_price
     StockQuote::Stock.json_quote(self.symbol)["quote"]["Ask"].to_f
   end
 
   def current_value
-    self.current_price * self.volume
+    current_price * volume
   end
 
   def cost_basis
-    self.volume * self.buy_price
+    volume * buy_price
   end
 
   def net_profit
-    self.current_value - self.cost_basis
+    current_value - cost_basis
   end
 
   def percent_change
-    ((self.net_profit / self.cost_basis) * 100).round(2)
+    (net_profit / cost_basis * 100).round(2)
   end
 end
