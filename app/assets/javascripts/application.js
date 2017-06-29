6841efaa3c64a93cacad4788983e6f28bc83140c
benjimon24 <benji102491@gmail.com>
2017-06-28 21:22:55 -0400

    change some colors, add logic to change color of change%

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,13 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+$(document).ready(function(){
+$(".percent-change").each(function(){
+  if ($(this).text().includes("-")) {
+    $(this).css({"color": "red"})
+  } else {
+    $(this).css({"color": "green"})
+  }
+})
+})
